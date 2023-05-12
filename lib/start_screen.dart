6ced836955366193ea6a7ff22c53536c217546e5
Alphin K Sajan <alphinksajan@gmail.com>
2023-05-12 12:18:32 +0530

    added a const

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,7 +12,7 @@ class StartScreen extends StatelessWidget
       child: Column(
         mainAxisSize : MainAxisSize.min,
         children: [
-        Image.asset('assets/images/quiz-logo.png',width:300,color:Color.fromARGB(150, 255, 255, 255)),
+        Image.asset('assets/images/quiz-logo.png',width:300,color:const Color.fromARGB(150, 255, 255, 255)),
         const SizedBox(height:80),
        Text('Solve this Quiz on Flutter !!',
         style: GoogleFonts.mavenPro(
